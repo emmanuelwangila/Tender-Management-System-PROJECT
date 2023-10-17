1ae3c221140d5ecb83ce84fabf34180276b9326c
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-10-17 15:19:51 +0300

    customize Ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,12 +23,12 @@ export default function Header() {
     }
   }, [location.search]);
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-white shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-blue-500'>TenderHub</span>
+            
           </h1>
         </Link>
         <form
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
+
 export default function About() {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto bg-white m-5 rounded-md  '>
-      <h1 className=' font-bold mb-4 text-blue-500 '>Tender Hub</h1>
-      <h2 className='font-bold font-sans text-blue-300 '> About Tender Hub</h2>
+      <h1 className=' font-bold text-2xl  mb-4 text-blue-500 flex justify-center '>Tender Hub</h1>
+      <h2 className='font-bold font-sans text-blue-500  flex justify-center'> About Tender Hub</h2>
+      <div className='text-gray-500 p-2 m-2 flex justify-center'>
+        <p>
+        TenderHub Management System is a software platform ,
+        designed to streamline and enhance the process of managing and participating,
+         in procurement tenders or requests for proposals (RFPs). 
+         This system is typically used by businesses, government organizations,
+          and other entities to facilitate the entire tendering process, 
+          from the creation and publication of tenders to the evaluation
+           and awarding of contracts.
+        </p>
+      </div>
+      <div>
+        {/* <img src={assets/hire.png } alt='tender-image' width={40} height={50}/>  */}
+
+      
+        </div>
      
       
       
