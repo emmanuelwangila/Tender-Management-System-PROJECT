499eb348408f627726b29e9288bfdd1b24e014b5
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-10-20 13:11:13 +0300

    style sign up page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -43,27 +43,27 @@ export default function SignIn() {
     }
   };
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+    <div className='p-3 max-w-lg mx-auto bg-white rounded-md  m-5 '>
+      <h1 className='text-3xl text-center font-semibold my-7 text-blue-500'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='email'
           placeholder='email'
-          className='border p-3 rounded-lg'
+          className='border p-3 rounded-lg w-3/4 mx-auto'
           id='email'
           onChange={handleChange}
         />
         <input
           type='password'
           placeholder='password'
-          className='border p-3 rounded-lg'
+          className='border p-3 rounded-lg w-3/4 mx-auto '
           id='password'
           onChange={handleChange}
         />
 
         <button
           disabled={loading}
-          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+          className='bg-blue-500 w-3/4 mx-auto text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
