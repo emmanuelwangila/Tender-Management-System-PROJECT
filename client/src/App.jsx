e8d7d4b709f15dd83e31cb2b14717df098f18dcb
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-11-09 10:27:58 +0300

    updates fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
-import CreateListing from "./pages/CreateListing";
+import CreateTender from "./pages/CreateTender";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
 import Search from "./pages/Search";
@@ -28,7 +28,7 @@ export default function App() {
 
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/create-tender" element={<CreateTender />} />
           <Route
             path="/update-listing/:listingId"
             element={<UpdateListing />}
