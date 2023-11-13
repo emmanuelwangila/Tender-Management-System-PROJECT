6a1282d43325ed118c93c26bec8a177b317c405e
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-11-13 21:22:56 +0300

    feat

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -67,7 +67,7 @@ export default function Home() {
           to={"/search"}
           className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
         >
-          Let's get started...
+          Let's get started ...Tender
         </Link>
       </div>
 
