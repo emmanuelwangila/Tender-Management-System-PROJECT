98c3ba848cc6a67ea02eed117033088b928d8055
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-11-14 15:48:40 +0300

    Firebase package

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "tender-management-system-1a9b6.firebaseapp.com",
+  projectId: "tender-management-system-1a9b6",
+  storageBucket: "tender-management-system-1a9b6.appspot.com",
+  messagingSenderId: "363775980447",
+  appId: "1:363775980447:web:f8617e5f2e7c70bf21c3aa",
+  measurementId: "G-EKHVYQ4YWX",
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
