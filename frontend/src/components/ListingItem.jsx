c2cabd7b469bc55c7ebca37e4eb5f9f7ecc70dbf
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-11-24 11:45:45 +0300

    styling

--- frontend/src/components/ListingItem.jsx
+++ frontend/src/components/ListingItem.jsx
@@ -10,7 +10,7 @@ export default function ListingItem({ listing }) {
             listing.imageUrls[0] ||
             "https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg"
           }
-          alt="listing cover"
+          alt="tender cover"
           className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
         <div className="p-3 flex flex-col gap-2 w-full">
@@ -18,7 +18,7 @@ export default function ListingItem({ listing }) {
             {listing.name}
           </p>
           <div className="flex items-center gap-1">
-            <MdLocationOn className="h-4 w-4 text-green-700" />
+            <MdLocationOn className="h-8 w-8 text-green-700" />
             <p className="text-sm text-teal-600 truncate w-full">
               {listing.address}
             </p>
@@ -26,7 +26,7 @@ export default function ListingItem({ listing }) {
           <p className="text-sm text-gray-600 line-clamp-2">
             {listing.description}
           </p>
-          <p className="text-slate-500 mt-2 font-semibold ">
+          <p className="text-blue-500  mt-2 font-bold ">
             $
             {listing.offer
               ? listing.discountPrice.toLocaleString("en-US")
@@ -34,16 +34,16 @@ export default function ListingItem({ listing }) {
             {listing.type === "rent" && " / month"}
           </p>
           <div className="text-slate-700 flex gap-4">
-            <div className="font-bold text-xs">
+            {/* <div className="font-bold text-xs">
               {listing.bedrooms > 1
                 ? `${listing.bedrooms} beds `
                 : `${listing.bedrooms} bed `}
-            </div>
-            <div className="font-bold text-xs">
+            </div> */}
+            {/* <div className="font-bold text-xs">
               {listing.bathrooms > 1
-                ? `${listing.bathrooms} baths `
-                : `${listing.bathrooms} bath `}
-            </div>
+                ? `${listing.bathrooms}  `
+                : `${listing.bathrooms} `}
+            </div> */}
           </div>
         </div>
       </Link>
